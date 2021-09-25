237c1e09cff3da8d77e44bd873b0a3f43a2c7210
Ethan Bautista <ethanb1@cpp.edu>
2021-09-25 14:24:31 -0700

    Ethan adding methods onto the header file

--- header.h
+++ header.h
@@ -1,2 +1,9 @@
-"hey"
-"lively wallpaper is nuts"
+#ifndef HEADER_H
+#define HEADER_H
+
+float numOfDays();
+float timeOfDepart();
+float timeOfArrival();
+float airFare();
+float carRental();
+#endif
