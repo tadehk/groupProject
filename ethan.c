95ea0ae1242616017dcc5ad59ce6cc6e8d829197
Tadeh Khajatourians <tadehk@cpp.edu>
2021-09-26 15:49:59 -0700

    trying to debug

--- ethan.c
+++ ethan.c
@@ -3,51 +3,73 @@
 
 //method that counts the number of days on vacation
 float numOfDays(){
-    float days;
-    printf("Please enter the number of days you are going on vacation: ");
-    scanf("%f", &days);
-    if (days<1){
-        printf("Invalid number of days, please try again.\n");
-        numOfDays();
+    float days = 0;
+    
+    while (days < 1){
+        printf("Please enter the number of days you are going on vacation: ");
+        scanf("%f", &days);
+       if (days < 1) {
+           printf("Invalid number of days, please try again.\n");
+       }
     }
-    printf("\nYou enetered: %f", days);
+    printf("\nYou entered: %f", days);
     return days;
 }
 //method that gets the time of departure
 float timeOfDepart(){
-    float timeD;
-    printf("\nWhat time are you leaving? Enter departure time in 24 hour format. ");
-    scanf("%f", &timeD);
-    if (timeD<0){
-        printf("Time can not be less than 0, please try agian.");
-        timeOfDepart();
-    }
-    printf("The time you entered is: %f", timeD);
+    float timeD = -1;
+
+    while (timeD < 0) {
+       printf("\nWhat time are you leaving? Enter departure time in 24 hour format. ");
+        scanf("%f", &timeD);
+
+        if (timeD<0){
+             printf("Time can not be less than 0, please try agian.\n");
+        }
+   } 
+    printf("The time you entered is: %f\n", timeD);
     return timeD;
 }
 //method that gets the time of arrival
 float timeOfArrival(){
-    float timeA;
-    printf("\n What time are you arriving? Enter arrival time in 24 hour format.");
-    scanf("%f", timeA);
-    if (timeA<0){
-        printf("Time can not be less than 0, please try again.");
-        timeOfArrival();
-    }
-    printf("The time you entered is: %f", timeA);
+    float timeA = -1;
+
+    while (timeA < 0) {
+       printf("\nWhat time are you leaving? Enter arrival time in 24 hour format. ");
+        scanf("%f", &timeA);
+
+        if (timeA<0){
+             printf("Time can not be less than 0, please try agian.\n");
+        }
+   } 
+    printf("The time you entered is: %f\n", timeA);
     return timeA;
 }
 //method that gets the airfare
 float airFare(){
-    float planeCost;
-    printf("\n How much is your plane ticket going to cost round-trip? ");
-    scanf("%f", planeCost);
+    float planeCost = -1;
+    while (planeCost < 0) {
+       printf("\nHow much does your plane cost round-trip? ");
+        scanf("%f", &planeCost);
+
+        if (planeCost<0){
+             printf("Amount cant be negative, please enter again:\n");
+        }
+   } 
+    printf("The amount you entered is: %f\n", planeCost);
     return planeCost;
 }
 //method that gets the car rental cost
 float carRental(){
-    float carCost;
-    printf("\n How much is your car rental going to cost? ");
-    scanf("%f", carCost);
+    float carCost = -1;
+    while (carCost < 0) {
+       printf("\nHow much does your car rental cost? ");
+        scanf("%f", &carCost);
+
+        if (carCost<0){
+             printf("Amount cant be negative, please enter again:\n");
+        }
+   } 
+    printf("The amount you entered is: %f\n", carCost);
     return carCost;
 }
